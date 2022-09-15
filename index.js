454fb850a6c1fe180494e7165069fd2b2d0f3b69
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-09-15 18:03:35 -0300

    html e javascript

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Tipos primitivos (string, number, boolean)
+ * Variáveis no Javascript
+ * Operadores matemáticos do Javascript
+ */
+
+
+// console.log("Hello World")
+// console.log('5')
+// console.log(43.76)
+
+let nome = "rodrigo"
+let idade = 18
+
+if (idade >= 18){
+  console.log("Pode votar "+nome)
+
+} else {
+  console.log("Não tem idade para votar "+nome)
+}
+
+let mensagem = "Javascript"
+
+console.log(mensagem)
+
+mensagem = "Soulcode"
+
+console.log(mensagem)
