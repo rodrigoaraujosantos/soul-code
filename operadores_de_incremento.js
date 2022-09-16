f4166e4a8c72fe00022b2721020e91c541089467
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-09-16 17:04:54 -0300

    op incremento, decremento

---
+++ operadores_de_incremento.js
@@ -0,0 +1,20 @@
+alert('Código Funcionando!') // mostrar uma mensagem para o usuário
+const nome_usuario = prompt('Olá, usuário, qual o seu nome?') // mostra uma mensagem e recebe um valor
+
+let num = Number(prompt('Olá, '+ nome_usuario + ' Digite algum número, por favor'))
+// const sucessor = num + 1
+// const antecessor = num -1
+
+// alert('O sucessor de '+num+ ' é ' + sucessor)
+// alert('O Antecessor de '+num+ ' é ' + antecessor)
+
+
+console.log(num++)
+console.log(num)
+console.log(++num)
+
+
+
+console.log(num--)
+console.log(num)
+console.log(--num)
