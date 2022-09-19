9839b3953ef6fde5e6a3dc7907292a52e34e5073
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-09-19 17:59:38 -0300

    operadores lógicos e comparação

---
+++ operadores_de_comparacao.js
@@ -0,0 +1,19 @@
+const msg = 'Eu sou uma mensagem'
+const msg2 = "Eu sou outra mensagem"
+console.log(msg == msg2)
+
+const num = 5
+const num2 = 5
+console.log(num == num2)
+
+let a = 6
+let b = 4
+console.log(a < 4)
+
+console.log(a != b )
+
+let c = 78
+let d = '78'
+console.log(c === d)
+console.log(c == d)
+
---
+++ operadores_logicos.js
@@ -0,0 +1,10 @@
+const media = (3.5 + 6 + 10)/3
+const presenca = 0.7
+
+console.log(media >= 7 && presenca >= 0.8)
+
+console.log(4 > 7 || 3 < 2)
+console.log(5 > 3.5 || 7 > 8)
+
+const valor = 5 < 3
+console.log(!valor)
