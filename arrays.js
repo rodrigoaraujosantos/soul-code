1b27b36570ce8b3e2ae186371e5e2ee00700930d
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-09-23 17:22:17 -0300

    desafio2: soma, maiorValor e menorValor

---
+++ arrays.js
@@ -0,0 +1,8 @@
+// let arr = new Array() // array vazio
+// let arr2 = new Array(5) // começa com 5 valores
+let arr3 = [10, 15, 9.8, 'array', 'teste', true, false]
+
+
+
+arr3[10] = 'teste'
+console.log(arr3)
