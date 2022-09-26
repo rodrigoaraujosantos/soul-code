3f4a8c82a87cbd7b3c8f8dfc53cd1a8705dd001e
rodrigoaraujosantos <araujo.1983@hotmail.com>
2022-09-26 15:54:11 -0300

    seis inteiros refeito

--- arrays.js
+++ arrays.js
@@ -1,8 +1,30 @@
 // let arr = new Array() // array vazio
 // let arr2 = new Array(5) // começa com 5 valores
-let arr3 = [10, 15, 9.8, 'array', 'teste', true, false]
+// let arr3 = [10, 15, 9.8, 'array', 'teste', true, false]
 
 
 
-arr3[10] = 'teste'
-console.log(arr3)
+// arr3[10] = 'teste'
+// console.log(arr3)
+
+
+const lista = ['João', 'Maria', 'Paulo', 'Pedro', 'Vitória', 'Augusto', 'Fábio', 'Larissa']
+
+console.log(lista)
+lista.push('Rodrigo')
+console.log(lista)
+lista.unshift('Gustavo')
+console.log(lista)
+lista.pop()
+console.log(lista)
+lista.shift()
+console.log(lista)
+
+
+console.log(lista.concat('Julio'))
+
+console.log(lista.join(' => '))
+
+
+
+
